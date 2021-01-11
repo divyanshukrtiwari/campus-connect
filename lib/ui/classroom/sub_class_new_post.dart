58ba804d72fba64990be844798e328ab3b12c5db
Divyanshu Kumar Tiwari <divyanshukrtiwari09@gmail.com>
2021-01-11 11:24:57 +0530

    added attach icon with popup in textfield

--- lib/ui/classroom/sub_class_new_post.dart
+++ lib/ui/classroom/sub_class_new_post.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SubClassNewPost extends StatefulWidget {
@@ -12,7 +13,11 @@ class SubClassNewPost extends StatefulWidget {
 class _SubClassNewPostState extends State<SubClassNewPost> {
   final _controller = TextEditingController();
 
+  final textFieldFocusNode = FocusNode();
+
   var enteredMsg = '';
+
+  bool popUp = false;
 
   void _sendMsg() async {
     FocusScope.of(context).unfocus();
@@ -37,43 +42,122 @@ class _SubClassNewPostState extends State<SubClassNewPost> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 65,
-      margin: EdgeInsets.only(top: 2),
+      height: popUp ? 220 : 80,
       padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-      child: Row(
-        children: <Widget>[
-          SizedBox(
-            width: 8,
-          ),
-          Expanded(
-            child: TextField(
-              textCapitalization: TextCapitalization.sentences,
-              autocorrect: true,
-              enableSuggestions: true,
-              controller: _controller,
-              decoration: InputDecoration(
-                filled: true,
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-                fillColor: Colors.grey.shade100,
-                labelText: 'enter message...',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
+      child: Column(
+        children: [
+          popUp
+              ? Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(16),
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.image,
+                                  size: 35,
+                                ),
+                                color: Colors.blue.shade500,
+                                onPressed: () {},
+                              ),
+                            ),
+                            Text('Image'),
+                          ],
+                        ),
+                        Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.picture_as_pdf,
+                                  size: 35,
+                                ),
+                                color: Colors.blue.shade500,
+                                onPressed: () {},
+                              ),
+                            ),
+                            Text('Document'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              : SizedBox(
+                  height: 0,
+                ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  textCapitalization: TextCapitalization.sentences,
+                  autocorrect: true,
+                  focusNode: textFieldFocusNode,
+                  enableSuggestions: true,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    suffixIcon: GestureDetector(
+                      child: Icon(
+                        Icons.attach_file,
+                        size: 25,
+                      ),
+                      onTap: () {
+                        if (!textFieldFocusNode.hasFocus)
+                          //textFieldFocusNode.unfocus();
+
+                          textFieldFocusNode.canRequestFocus = false;
+
+                        setState(() {
+                          if (popUp) {
+                            popUp = false;
+                          } else {
+                            popUp = true;
+                          }
+                        });
+
+                        Future.delayed(Duration(milliseconds: 100), () {
+                          textFieldFocusNode.canRequestFocus = true;
+                        });
+                      },
+                    ),
+                    filled: true,
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    fillColor: Colors.grey.shade100,
+                    labelText: 'enter message...',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      enteredMsg = value;
+                    });
+                  },
                 ),
               ),
-              onChanged: (value) {
-                setState(() {
-                  enteredMsg = value;
-                });
-              },
-            ),
+              IconButton(
+                icon: Icon(
+                  Icons.send,
+                  color: Theme.of(context).primaryColor,
+                ),
+                onPressed: enteredMsg.trim().isEmpty ? null : _sendMsg,
+              )
+            ],
           ),
-          IconButton(
-            icon: Icon(
-              Icons.send,
-              color: Theme.of(context).primaryColor,
-            ),
-            onPressed: enteredMsg.trim().isEmpty ? null : _sendMsg,
-          )
         ],
       ),
     );
